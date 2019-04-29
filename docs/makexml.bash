daab7d343b25e2863c83303eec029f49ed961e69
Francis Bond <bond@ieee.org>
2019-04-29 11:28:43 +0800

    added a license for the pygment css

--- docs/makexml.bash
+++ docs/makexml.bash
@@ -2,6 +2,10 @@
 ## I ran this then put the snippet in the template.html file
 ## and deleted snippets.html
 ##
+style=lovelace
+printf -v license '/* BSD-licensed. See: https://bitbucket.org/birkenfeld/pygments-main/src/default/pygments/styles/%s.py */' "${style}"
+echo "${license}"
 
 pygmentize -f html -o snippets.html snippets.xml
-pygmentize -S lovelace  -f html  > static/pygment.css
+echo "${license}" >  static/pygment.css
+pygmentize -S ${style}  -f html  >> static/pygment.css
