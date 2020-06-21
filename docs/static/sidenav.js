893bfd8c1dda4d8673cf2084216cb2e3a05637d4
yoyo-go <56577574+yoyo-go@users.noreply.github.com>
2020-06-21 18:26:49 +0800

    revise the sidenav
    1. remove internal name in sidenav;
    2. change down arrow to plus/minus icon in sidenav;
    3. add back the Table of Content section;

--- docs/static/sidenav.js
+++ docs/static/sidenav.js
@@ -10,26 +10,74 @@ window.onclick = function(event) {
     }
   }
 }
+var btn1Status=0;
 $(document).ready(function () {
+	
 	$("#dp-btn1").click(function () {
 		$("#dropList1").toggle();
+		if(btn1Status==0){
+			btn1Status=1;
+			$("#sp1-status").attr("class","fa fa-minus-circle");
+			console.log(btn1Status);
+		}
+		else{
+			btn1Status=0;
+			$("#sp1-status").attr("class","fa fa-plus-circle");
+			console.log(btn1Status);
+		}
 	})
 })
 
+var btn2Status=0;
 $(document).ready(function () {
+	
 	$("#dp-btn2").click(function () {
 		$("#dropList2").toggle();
+		if(btn2Status==0){
+			btn2Status=1;
+			$("#sp2-status").attr("class","fa fa-minus-circle");
+			console.log(btn2Status);
+		}
+		else{
+			btn2Status=0;
+			$("#sp2-status").attr("class","fa fa-plus-circle");
+			console.log(btn2Status);
+		}
 	})
 })
 
+var btn3Status=0;
 $(document).ready(function () {
+	
 	$("#dp-btn3").click(function () {
 		$("#dropList3").toggle();
+		if(btn3Status==0){
+			btn3Status=1;
+			$("#sp3-status").attr("class","fa fa-minus-circle");
+			console.log(btn3Status);
+		}
+		else{
+			btn3Status=0;
+			$("#sp3-status").attr("class","fa fa-plus-circle");
+			console.log(btn3Status);
+		}
 	})
 })
 
+var btn4Status=0;
 $(document).ready(function () {
+	
 	$("#dp-btn4").click(function () {
 		$("#dropList4").toggle();
+		if(btn4Status==0){
+			btn4Status=1;
+			$("#sp4-status").attr("class","fa fa-minus-circle");
+			console.log(btn4Status);
+		}
+		else{
+			btn4Status=0;
+			$("#sp4-status").attr("class","fa fa-plus-circle");
+			console.log(btn4Status);
+		}
 	})
 })
