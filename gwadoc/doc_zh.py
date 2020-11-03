f475b33f8fc02e3b40c07653b658eb3ad96cdcab
Francis Bond <bond@ieee.org>
2020-11-03 17:30:27 +0800

    Added automatically made doc stub for Chinese

---
+++ gwadoc/doc_zh.py
@@ -0,0 +1,627 @@
+# -*- coding: utf-8 -*-
+###
+### Documentation for zh
+###
+
+# Pre-seeded by XXX on YYY
+#
+# Un-comment lines when you have checked them
+
+from gwadoc import relations
+
+
+
+### constitutive 
+
+# relations.constitutive.name.zh = ""
+# relations.constitutive.df.zh = ""
+# relations.constitutive.ex.zh = ""
+
+
+### hyponym i69570
+
+# relations.hyponym.name.zh = ""
+# relations.hyponym.df.zh = ""
+# relations.hyponym.ex.zh = ""
+
+
+### hypernym i69569
+
+# relations.hypernym.name.zh = ""
+# relations.hypernym.df.zh = ""
+# relations.hypernym.ex.zh = ""
+
+
+### instance_hyponym i75102
+
+# relations.instance_hyponym.name.zh = "事例, 例子, 场合, 实例, 情况"
+# relations.instance_hyponym.df.zh = ""
+# relations.instance_hyponym.ex.zh = ""
+
+
+### instance_hypernym 
+
+# relations.instance_hypernym.name.zh = ""
+# relations.instance_hypernym.df.zh = ""
+# relations.instance_hypernym.ex.zh = ""
+
+
+### antonym i69547
+
+# relations.antonym.name.zh = "反义词"
+# relations.antonym.df.zh = ""
+# relations.antonym.ex.zh = ""
+
+
+### eq_synonym i69607
+
+# relations.eq_synonym.name.zh = ""
+# relations.eq_synonym.df.zh = ""
+# relations.eq_synonym.ex.zh = ""
+
+
+### similar i13187
+
+# relations.similar.name.zh = "相似+的"
+# relations.similar.df.zh = ""
+# relations.similar.ex.zh = ""
+
+
+### meronym i69575
+
+# relations.meronym.name.zh = ""
+# relations.meronym.df.zh = ""
+# relations.meronym.ex.zh = ""
+
+
+### holonym i69567
+
+# relations.holonym.name.zh = ""
+# relations.holonym.df.zh = ""
+# relations.holonym.ex.zh = ""
+
+
+### mero_location 
+
+# relations.mero_location.name.zh = ""
+# relations.mero_location.df.zh = ""
+# relations.mero_location.ex.zh = ""
+
+
+### holo_location 
+
+# relations.holo_location.name.zh = ""
+# relations.holo_location.df.zh = ""
+# relations.holo_location.ex.zh = ""
+
+
+### mero_member 
+
+# relations.mero_member.name.zh = ""
+# relations.mero_member.df.zh = ""
+# relations.mero_member.ex.zh = ""
+
+
+### holo_member 
+
+# relations.holo_member.name.zh = ""
+# relations.holo_member.df.zh = ""
+# relations.holo_member.ex.zh = ""
+
+
+### mero_part 
+
+# relations.mero_part.name.zh = ""
+# relations.mero_part.df.zh = ""
+# relations.mero_part.ex.zh = ""
+
+
+### holo_part 
+
+# relations.holo_part.name.zh = ""
+# relations.holo_part.df.zh = ""
+# relations.holo_part.ex.zh = ""
+
+
+### mero_portion 
+
+# relations.mero_portion.name.zh = ""
+# relations.mero_portion.df.zh = ""
+# relations.mero_portion.ex.zh = ""
+
+
+### holo_portion 
+
+# relations.holo_portion.name.zh = ""
+# relations.holo_portion.df.zh = ""
+# relations.holo_portion.ex.zh = ""
+
+
+### mero_substance 
+
+# relations.mero_substance.name.zh = ""
+# relations.mero_substance.df.zh = ""
+# relations.mero_substance.ex.zh = ""
+
+
+### holo_substance 
+
+# relations.holo_substance.name.zh = ""
+# relations.holo_substance.df.zh = ""
+# relations.holo_substance.ex.zh = ""
+
+
+### other i11342
+
+# relations.other.name.zh = "其他+的, 其它, 另外+的"
+# relations.other.df.zh = ""
+# relations.other.ex.zh = ""
+
+
+### also 
+
+# relations.also.name.zh = ""
+# relations.also.df.zh = ""
+# relations.also.ex.zh = ""
+
+
+### state_of i35578
+
+# relations.state_of.name.zh = ""
+# relations.state_of.df.zh = ""
+# relations.state_of.ex.zh = ""
+
+
+### be_in_state 
+
+# relations.be_in_state.name.zh = ""
+# relations.be_in_state.df.zh = ""
+# relations.be_in_state.ex.zh = ""
+
+
+### causes i35561
+
+# relations.causes.name.zh = ""
+# relations.causes.df.zh = ""
+# relations.causes.ex.zh = ""
+
+
+### is_caused_by 
+
+# relations.is_caused_by.name.zh = ""
+# relations.is_caused_by.df.zh = ""
+# relations.is_caused_by.ex.zh = ""
+
+
+### subevent 
+
+# relations.subevent.name.zh = ""
+# relations.subevent.df.zh = ""
+# relations.subevent.ex.zh = ""
+
+
+### is_subevent_of 
+
+# relations.is_subevent_of.name.zh = ""
+# relations.is_subevent_of.df.zh = ""
+# relations.is_subevent_of.ex.zh = ""
+
+
+### manner_of i62791
+
+# relations.manner_of.name.zh = "态度, 方式, 风度"
+# relations.manner_of.df.zh = ""
+# relations.manner_of.ex.zh = ""
+
+
+### in_manner 
+
+# relations.in_manner.name.zh = ""
+# relations.in_manner.df.zh = ""
+# relations.in_manner.ex.zh = ""
+
+
+### attribute i35577
+
+# relations.attribute.name.zh = ""
+# relations.attribute.df.zh = ""
+# relations.attribute.ex.zh = ""
+
+
+### restricts i22888
+
+# relations.restricts.name.zh = "拘束, 束缚, 约束, 限制, 限定"
+# relations.restricts.df.zh = ""
+# relations.restricts.ex.zh = ""
+
+
+### restricted_by 
+
+# relations.restricted_by.name.zh = ""
+# relations.restricted_by.df.zh = ""
+# relations.restricted_by.ex.zh = ""
+
+
+### classifies i25399
+
+# relations.classifies.name.zh = ""
+# relations.classifies.df.zh = ""
+# relations.classifies.ex.zh = ""
+
+
+### classified_by i25002
+
+# relations.classified_by.name.zh = ""
+# relations.classified_by.df.zh = ""
+# relations.classified_by.ex.zh = ""
+
+
+### entails i34846
+
+# relations.entails.name.zh = "暗指, 暗示"
+# relations.entails.df.zh = ""
+# relations.entails.ex.zh = ""
+
+
+### is_entailed_by 
+
+# relations.is_entailed_by.name.zh = ""
+# relations.is_entailed_by.df.zh = ""
+# relations.is_entailed_by.ex.zh = ""
+
+
+### domain 
+
+# relations.domain.name.zh = ""
+# relations.domain.df.zh = ""
+# relations.domain.ex.zh = ""
+
+
+### has_domain 
+
+# relations.has_domain.name.zh = ""
+# relations.has_domain.df.zh = ""
+# relations.has_domain.ex.zh = ""
+
+
+### domain_topic 
+
+# relations.domain_topic.name.zh = ""
+# relations.domain_topic.df.zh = ""
+# relations.domain_topic.ex.zh = ""
+
+
+### has_domain_topic 
+
+# relations.has_domain_topic.name.zh = ""
+# relations.has_domain_topic.df.zh = ""
+# relations.has_domain_topic.ex.zh = ""
+
+
+### domain_region 
+
+# relations.domain_region.name.zh = ""
+# relations.domain_region.df.zh = ""
+# relations.domain_region.ex.zh = ""
+
+
+### has_domain_region 
+
+# relations.has_domain_region.name.zh = ""
+# relations.has_domain_region.df.zh = ""
+# relations.has_domain_region.ex.zh = ""
+
+
+### exemplifies i108797
+
+# relations.exemplifies.name.zh = "马纳特"
+# relations.exemplifies.df.zh = ""
+# relations.exemplifies.ex.zh = ""
+
+
+### is_exemplified_by 
+
+# relations.is_exemplified_by.name.zh = ""
+# relations.is_exemplified_by.df.zh = ""
+# relations.is_exemplified_by.ex.zh = ""
+
+
+### role i64041
+
+# relations.role.name.zh = "作用, 功能, 机能"
+# relations.role.df.zh = ""
+# relations.role.ex.zh = ""
+
+
+### involved i8315
+
+# relations.involved.name.zh = ""
+# relations.involved.df.zh = ""
+# relations.involved.ex.zh = ""
+
+
+### agent i69754
+
+# relations.agent.name.zh = "主动角色, 施事"
+# relations.agent.df.zh = ""
+# relations.agent.ex.zh = ""
+
+
+### involved_agent 
+
+# relations.involved_agent.name.zh = ""
+# relations.involved_agent.df.zh = ""
+# relations.involved_agent.ex.zh = ""
+
+
+### patient i69753
+
+# relations.patient.name.zh = "受事, 被动角色"
+# relations.patient.df.zh = ""
+# relations.patient.ex.zh = ""
+
+
+### involved_patient 
+
+# relations.involved_patient.name.zh = ""
+# relations.involved_patient.df.zh = ""
+# relations.involved_patient.ex.zh = ""
+
+
+### result i69759
+
+# relations.result.name.zh = ""
+# relations.result.df.zh = ""
+# relations.result.ex.zh = ""
+
+
+### involved_result 
+
+# relations.involved_result.name.zh = ""
+# relations.involved_result.df.zh = ""
+# relations.involved_result.ex.zh = ""
+
+
+### instrument i69756
+
+# relations.instrument.name.zh = ""
+# relations.instrument.df.zh = ""
+# relations.instrument.ex.zh = ""
+
+
+### involved_instrument 
+
+# relations.involved_instrument.name.zh = ""
+# relations.involved_instrument.df.zh = ""
+# relations.involved_instrument.ex.zh = ""
+
+
+### location i35580
+
+# relations.location.name.zh = "位置"
+# relations.location.df.zh = ""
+# relations.location.ex.zh = ""
+
+
+### involved_location 
+
+# relations.involved_location.name.zh = ""
+# relations.involved_location.df.zh = ""
+# relations.involved_location.ex.zh = ""
+
+
+### direction i82556
+
+# relations.direction.name.zh = ""
+# relations.direction.df.zh = ""
+# relations.direction.ex.zh = ""
+
+
+### involved_direction 
+
+# relations.involved_direction.name.zh = ""
+# relations.involved_direction.df.zh = ""
+# relations.involved_direction.ex.zh = ""
+
+
+### target_direction i82007
+
+# relations.target_direction.name.zh = "目的地"
+# relations.target_direction.df.zh = ""
+# relations.target_direction.ex.zh = ""
+
+
+### involved_target_direction 
+
+# relations.involved_target_direction.name.zh = ""
+# relations.involved_target_direction.df.zh = ""
+# relations.involved_target_direction.ex.zh = ""
+
+
+### source_direction i81759
+
+# relations.source_direction.name.zh = "开始, 开端, 根, 根源, 起因, 起源"
+# relations.source_direction.df.zh = ""
+# relations.source_direction.ex.zh = ""
+
+
+### involved_source_direction 
+
+# relations.involved_source_direction.name.zh = ""
+# relations.involved_source_direction.df.zh = ""
+# relations.involved_source_direction.ex.zh = ""
+
+
+### co_role 
+
+# relations.co_role.name.zh = ""
+# relations.co_role.df.zh = ""
+# relations.co_role.ex.zh = ""
+
+
+### co_agent_patient 
+
+# relations.co_agent_patient.name.zh = ""
+# relations.co_agent_patient.df.zh = ""
+# relations.co_agent_patient.ex.zh = ""
+
+
+### co_patient_agent 
+
+# relations.co_patient_agent.name.zh = ""
+# relations.co_patient_agent.df.zh = ""
+# relations.co_patient_agent.ex.zh = ""
+
+
+### co_agent_instrument 
+
+# relations.co_agent_instrument.name.zh = ""
+# relations.co_agent_instrument.df.zh = ""
+# relations.co_agent_instrument.ex.zh = ""
+
+
+### co_instrument_agent 
+
+# relations.co_instrument_agent.name.zh = ""
+# relations.co_instrument_agent.df.zh = ""
+# relations.co_instrument_agent.ex.zh = ""
+
+
+### co_agent_result 
+
+# relations.co_agent_result.name.zh = ""
+# relations.co_agent_result.df.zh = ""
+# relations.co_agent_result.ex.zh = ""
+
+
+### co_result_agent 
+
+# relations.co_result_agent.name.zh = ""
+# relations.co_result_agent.df.zh = ""
+# relations.co_result_agent.ex.zh = ""
+
+
+### co_patient_instrument 
+
+# relations.co_patient_instrument.name.zh = ""
+# relations.co_patient_instrument.df.zh = ""
+# relations.co_patient_instrument.ex.zh = ""
+
+
+### co_instrument_patient 
+
+# relations.co_instrument_patient.name.zh = ""
+# relations.co_instrument_patient.df.zh = ""
+# relations.co_instrument_patient.ex.zh = ""
+
+
+### co_result_instrument 
+
+# relations.co_result_instrument.name.zh = ""
+# relations.co_result_instrument.df.zh = ""
+# relations.co_result_instrument.ex.zh = ""
+
+
+### co_instrument_result 
+
+# relations.co_instrument_result.name.zh = ""
+# relations.co_instrument_result.df.zh = ""
+# relations.co_instrument_result.ex.zh = ""
+
+
+### participle 
+
+# relations.participle.name.zh = ""
+# relations.participle.df.zh = ""
+# relations.participle.ex.zh = ""
+
+
+### pertainym 
+
+# relations.pertainym.name.zh = ""
+# relations.pertainym.df.zh = ""
+# relations.pertainym.ex.zh = ""
+
+
+### derivation i71936
+
+# relations.derivation.name.zh = ""
+# relations.derivation.df.zh = ""
+# relations.derivation.ex.zh = ""
+
+
+### simple_aspect 
+
+# relations.simple_aspect.name.zh = ""
+# relations.simple_aspect.df.zh = ""
+# relations.simple_aspect.ex.zh = ""
+
+
+### secondary_aspect 
+
+# relations.secondary_aspect.name.zh = ""
+# relations.secondary_aspect.df.zh = ""
+# relations.secondary_aspect.ex.zh = ""
+
+
+### feminine_form_of 
+
+# relations.feminine_form_of.name.zh = ""
+# relations.feminine_form_of.df.zh = ""
+# relations.feminine_form_of.ex.zh = ""
+
+
+### masculine_form_of 
+
+# relations.masculine_form_of.name.zh = ""
+# relations.masculine_form_of.df.zh = ""
+# relations.masculine_form_of.ex.zh = ""
+
+
+### young_form_of 
+
+# relations.young_form_of.name.zh = ""
+# relations.young_form_of.df.zh = ""
+# relations.young_form_of.ex.zh = ""
+
+
+### diminutive_of 
+
+# relations.diminutive_of.name.zh = ""
+# relations.diminutive_of.df.zh = ""
+# relations.diminutive_of.ex.zh = ""
+
+
+### augmentative_of 
+
+# relations.augmentative_of.name.zh = ""
+# relations.augmentative_of.df.zh = ""
+# relations.augmentative_of.ex.zh = ""
+
+
+### anto_gradable 
+
+# relations.anto_gradable.name.zh = ""
+# relations.anto_gradable.df.zh = ""
+# relations.anto_gradable.ex.zh = ""
+
+
+### anto_simple 
+
+# relations.anto_simple.name.zh = ""
+# relations.anto_simple.df.zh = ""
+# relations.anto_simple.ex.zh = ""
+
+
+### anto_converse 
+
+# relations.anto_converse.name.zh = ""
+# relations.anto_converse.df.zh = ""
+# relations.anto_converse.ex.zh = ""
+
+
+### ir_synonym 
+
+# relations.ir_synonym.name.zh = ""
+# relations.ir_synonym.df.zh = ""
+# relations.ir_synonym.ex.zh = ""
